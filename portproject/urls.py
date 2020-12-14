5308328c885906864988a99c51cc0e6355e15d3f
nickinfo1 <nickinfo1@yandex.ru>
2020-12-14 19:35:17 +0800

    add dir templates and html file

--- portproject/urls.py
+++ portproject/urls.py
@@ -18,6 +18,5 @@ from django.urls import path
 from firstapp import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('admin/', admin.site.urls),
+    path('', views.index),
 ]
