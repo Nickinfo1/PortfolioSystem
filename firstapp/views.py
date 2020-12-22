ab8e312db71e96ccc8077021e135d6670bea7ca6
Nickinfo1 <nickinfo1@yandex.ru>
2020-12-22 19:46:35 +0500

    added return some variables to the web page

--- firstapp/views.py
+++ firstapp/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "main.html")
+    one = {2, 23, 34+12}
+    two = "my text"
+    data = {"header":one, "message":two}
+    return render(request, "main.html", context=data)
